4215c4723b8c8e5db9982ecf2bb1a06527a819eb
Matt Mason <masonmj10@gmail.com>
2020-07-09 17:58:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves # code an argument here
+ new_dwarves = []
+  dwarves.each.with_index(1) do |name, index|
+    new_dwarves << "#{index}. #{name}"
+  end
+  puts new_dwarves.join(" ")
 end
 
 def summon_captain_planet# code an argument here
