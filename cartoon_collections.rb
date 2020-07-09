7b4214bc352f9121045518d36fa73c2c86595890
Matt Mason <masonmj10@gmail.com>
2020-07-09 18:04:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves # code an argument here
  new_dwarves = []
- dwarves.each.with_index do |dwarf, i|
+ dwarves.each.with_index(1) do |dwarf, i|
     "#{i + 1}. #{dwarf}"
   end.join(" ")
 end
