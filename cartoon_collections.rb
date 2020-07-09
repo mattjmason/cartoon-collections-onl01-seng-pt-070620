250916f7ec493bd724907052400723837db002a1
Matt Mason <masonmj10@gmail.com>
2020-07-09 18:07:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,16 +6,18 @@ def roll_call_dwarves # code an argument here
   puts new_dwarves.join(" ")
 end
 
-dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
 
 def summon_captain_planet# code an argument here
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |words|
+    words.size > 4
+  end
 end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
